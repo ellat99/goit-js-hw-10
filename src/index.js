0d16aca93b4b2fcc26a9c1d67f33b9a5577f969c
tle09 <elena_larisa_t@yahoo.com>
2023-11-19 14:43:35 +0200

    task10

--- src/index.js
+++ src/index.js
@@ -6,17 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const error = document.querySelector('.error');
   const catInfo = document.querySelector('.cat-info');
 
-  // Adăugați această linie pentru a seta lățimea dorită pentru breed-select
-  breedSelect.style.width = '200px'; // Puteți ajusta acest număr la dimensiunea dorită
+  breedSelect.style.width = '200px';
 
-  // Ascundem loaderul și eroarea inițial
   loader.style.display = 'none';
   error.style.display = 'none';
 
-  // Încărcăm lista de rase la încărcarea paginii
   fetchBreeds()
     .then(breeds => {
-      loader.style.display = 'none'; // Ascundem loaderul la finalizarea cererii
+      loader.style.display = 'none';
       breeds.forEach(breed => {
         const option = document.createElement('option');
         option.value = breed.id;
@@ -25,26 +22,24 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     })
     .catch(error => {
-      loader.style.display = 'none'; // Ascundem loaderul în caz de eroare
-      error.style.display = 'block'; // Afișăm eroarea
+      loader.style.display = 'none';
+      error.style.display = 'block';
       console.error('Error fetching breeds:', error);
     });
 
-  // Ascultăm schimbările în selector și efectuăm cererea pentru informații despre pisică
   breedSelect.addEventListener('change', event => {
     const selectedBreedId = event.target.value;
 
     if (selectedBreedId) {
-      loader.style.display = 'block'; // Afișăm loaderul în timpul cererii
-      error.style.display = 'none'; // Ascundem eroarea, dacă era afișată anterior
-      catInfo.style.display = 'none'; // Ascundem informațiile despre pisică în timpul cererii
+      loader.style.display = 'block';
+      error.style.display = 'none';
+      catInfo.style.display = 'none';
 
       fetchCatByBreed(selectedBreedId)
         .then(catData => {
-          // Afișăm informațiile despre pisică
           const catImage = document.createElement('img');
           catImage.src = catData.url;
-          catInfo.innerHTML = ''; // Golește conținutul existent
+          catInfo.innerHTML = '';
           catInfo.appendChild(catImage);
           const catName = document.createElement('p');
           catName.textContent = catData.breeds[0].name;
@@ -57,13 +52,13 @@ document.addEventListener('DOMContentLoaded', () => {
           catInfo.appendChild(catTemperament);
         })
         .catch(error => {
-          loader.style.display = 'none'; // Ascundem loaderul în caz de eroare
-          error.style.display = 'block'; // Afișăm eroarea
+          loader.style.display = 'none';
+          error.style.display = 'block';
           console.error('Error fetching cat information:', error);
         })
         .finally(() => {
-          loader.style.display = 'none'; // Ascundem loaderul după finalizarea cererii
-          catInfo.style.display = 'block'; // Afișăm informațiile despre pisică după finalizarea cererii
+          loader.style.display = 'none';
+          catInfo.style.display = 'block';
         });
     }
   });
